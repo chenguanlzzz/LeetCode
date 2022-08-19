a8a2c329960e49b62fd616a73eec12cd93efc935
chenguanlzzz <paul840824@gmail.com>
2022-08-19 23:21:31 +0800

    init git

---
+++ 110.balanced-binary-tree.java
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=110 lang=java
+ *
+ * [110] Balanced Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        
+    }
+    private int getHeight(TreeNode root) {
+        if (root == null) return 0;
+        else if (Math.abs(getHeight(root.left) - getHeight(root.right)))
+    }
+}
+// @lc code=end
+
