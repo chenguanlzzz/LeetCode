bbdee47919cd96415fe970b5e3315d2e94f33b42
chenguanlzzz <paul840824@gmail.com>
2022-08-19 23:31:33 +0800

    finish prob.110

--- 110.balanced-binary-tree.java
+++ 110.balanced-binary-tree.java
@@ -22,11 +22,14 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        
+        if (getHeight(root) == -1) return false;
+        return true;
     }
     private int getHeight(TreeNode root) {
         if (root == null) return 0;
-        else if (Math.abs(getHeight(root.left) - getHeight(root.right)))
+        else if (getHeight(root.left) == -1 || getHeight(root.right) == -1) return -1;
+        else if (Math.abs(getHeight(root.left) - getHeight(root.right)) > 1) return -1;
+        else return 1 + Math.max(getHeight(root.left), getHeight(root.right));
     }
 }
 // @lc code=end
